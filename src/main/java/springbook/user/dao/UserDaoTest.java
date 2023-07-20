043366bd430332930dc5270ab5eca03b2faddeb5
DESKTOP-N82UAAS\55316 <jinwook5062@gmail.com>
2023-07-20 21:54:50 +0900

    JUnit 프레임워크를 이용한 UserDaoTest 전환

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -1,18 +1,22 @@
 package springbook.user.dao;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import springbook.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class UserDaoTest {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    @Test
+    public void addAndGet() throws SQLException {
 
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        ApplicationContext context = new GenericXmlApplicationContext("dataSourceContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("dataSourceContext.xml");
+
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
@@ -22,12 +26,20 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공!");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
 
-        System.out.println(user2.getId() + " 조회 성공!");
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+//        if (!user.getName().equals(user2.getName())){
+//            System.out.println("테스트 실패 (name)");
+//        }
+//        else if (!user.getPassword().equals(user2.getPassword())){
+//            System.out.println("테스트 실패 (password)");
+//        }
+//        else {
+//            System.out.println("조회 테스트 성공");
+//        }
     }
+
 }
