c3ed8ec9d0126bcc5022a8766832a576a09549fc
DESKTOP-N82UAAS\55316 <jinwook5062@gmail.com>
2023-07-27 15:48:35 +0900

    JdbcTemplate를 이용하여 완성된 UserDao 코드

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,114 +1,60 @@
 package springbook.user.dao;
 
-import org.springframework.dao.EmptyResultDataAccessException;
-import springbook.user.context.JdbcContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-
-        this.jdbcContext.setDataSource(dataSource);
-
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    };
 
     public void add(final User user) throws ClassNotFoundException, SQLException{
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) value(?,?,?)");
-                ps.setString(1, user.getId());	//외부 add메소드의 user 변수에 접근 가능
-                ps.setString(2, user.getName()); //외부 add메소드의 user 변수에 접근 가능
-                ps.setString(3, user.getPassword()); //외부 add메소드의 user 변수에 접근 가능
-                return ps;
-            }
-        });
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword());
 
-        }
-
+    }
 
     public User get(String id) throws SQLException{
 
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?");
-
-        ps.setString(1, id);
-
-        ResultSet resultSet = ps.executeQuery();
-
-        User user = null;
-        if (resultSet.next()){
-            user = new User();
-            user.setId(resultSet.getString("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
-        }
-
-        resultSet.close();
-        ps.close();
-        c.close();
-
-        if (user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+        return this.jdbcTemplate.queryForObject(
+                "select * from users where id = ?",
+                new Object[]{id},
+                this.userMapper);
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+        this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("select count(*) from users");
-
-            resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            return resultSet.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (resultSet != null){
-                try {
-                    resultSet.close();
-                } catch (SQLException e){
-
-                }
-            }
-            if (preparedStatement != null){
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e){
-
-                }
-            }
-            if (connection != null){
-                try {
-                    connection.close();
-                } catch (SQLException e){
-
-                }
-            }
-        }
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
     }
 
-
+    public List<User> getAll() {
+        return this.jdbcTemplate.query(
+                "select * from users order by id",
+                this.userMapper);
+    }
 
 }
--- src/main/java/springbook/user/test/UserDaoTest.java
+++ src/main/java/springbook/user/test/UserDaoTest.java
@@ -10,6 +10,7 @@ import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -31,9 +32,9 @@ public class UserDaoTest {
     @Before
     public void setUp(){
 
-        this.user1 = new User("whiteship", "백사장", "spring1");
-        this.user2 = new User("leeman", "이사람", "spring2");
-        this.user3 = new User("kimid", "김인간", "spring3");
+        this.user1 = new User("gyumee", "백사장", "spring1");
+        this.user2 = new User("leegw700", "이사람", "spring2");
+        this.user3 = new User("bumjin", "김인간", "spring3");
 
         dao = new UserDao();
         DataSource dataSource = new SingleConnectionDataSource(
@@ -91,6 +92,40 @@ public class UserDaoTest {
         assertThat(dao.getCount(), is(0));
 
         dao.get("unknown_id");
+    }
+
+    @Test
+    public void getAllTest() throws SQLException, ClassNotFoundException{
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user3, users3.get(0));
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user2, users3.get(2));
+
+
+    }
+
+    private void checkSameUser(User user1, User user2){
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 
 }
