3c1dabc876040777f72093a7df3b26534bea570a
shouting crow <jinwook5062@gmail.com>
2023-08-10 01:45:07 +0900

    프록시 팩토리 빈을 적용한 코드

---
+++ src/main/java/springbook/learningtest/factoryBean/MessageFactoryBean.java
@@ -0,0 +1,26 @@
+package springbook.learningtest.factoryBean;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class MessageFactoryBean implements FactoryBean {
+    String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public Message getObject() throws Exception {
+        return Message.newMessage(text);
+    }
+
+    @Override
+    public Class<? extends Message> getObjectType() {
+        return Message.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
--- src/main/java/springbook/user/service/UserServiceTest.java
+++ src/main/java/springbook/user/service/UserServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.annotation.DirtiesContext;
@@ -14,10 +15,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
-import springbook.user.service.handler.TransactionHandler;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,6 +37,8 @@ public class UserServiceTest {
     @Autowired PlatformTransactionManager transactionManager;
     @Autowired MailSender mailSender;
     @Autowired UserServiceImpl userServiceImpl;
+    @Autowired
+    ApplicationContext context;
 
     List<User> users;	// test fixture
 
@@ -111,22 +112,27 @@ public class UserServiceTest {
     }
 
     @Test
+    @DirtiesContext
     public void upgradeAllOrNothing() throws Exception {
 
         TestUserService testUserService = new TestUserService(users.get(3).getId());
         testUserService.setUserDao(this.userDao);
         testUserService.setMailSender(this.mailSender);
 
-        TransactionHandler txHandler = new TransactionHandler();
-        txHandler.setTarget(testUserService);
-        txHandler.setTransactionManager(transactionManager);
-        txHandler.setPattern("upgradeLevels");
+        TxProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", TxProxyFactoryBean.class);
+        txProxyFactoryBean.setTarget(testUserService);
+        UserService txUserService = (UserService) txProxyFactoryBean.getObject();
 
-        UserService txUserService = (UserService) Proxy.newProxyInstance(
-                getClass().getClassLoader(),
-                new Class[] {UserService.class},
-                txHandler
-        ) ;
+//        TransactionHandler txHandler = new TransactionHandler();
+//        txHandler.setTarget(testUserService);
+//        txHandler.setTransactionManager(transactionManager);
+//        txHandler.setPattern("upgradeLevels");
+
+//        UserService txUserService = (UserService) Proxy.newProxyInstance(
+//                getClass().getClassLoader(),
+//                new Class[] {UserService.class},
+//                txHandler
+//        ) ;
 
         userDao.deleteAll();
         for(User user : users) userDao.add(user);
