679b67245a9ffd0c656e88e4cb2ea6c7071f4bd5
shouting crow <jinwook5062@gmail.com>
2023-08-22 17:19:32 +0900

    OXM을 통해 JAXB와 같은 API를 쉽게 사용할 수 있고 리소스 추상화를 학습한 코드

---
+++ src/main/java/springbook/learningtest/spring/oxm/OxmTest.java
@@ -0,0 +1,39 @@
+package springbook.learningtest.spring.oxm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.sqlService.jaxb.SqlType;
+import springbook.user.sqlService.jaxb.Sqlmap;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/OxmTest-context.xml")
+public class OxmTest {
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallerSqlMap() throws XmlMappingException, IOException{
+        Source xmlSource = new StreamSource(
+                getClass().getResourceAsStream("sqlmap.xml")
+        );
+
+        Sqlmap sqlmap = (Sqlmap)this.unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+    }
+}
